ecfc8d300d60610cdd7ef90b6c2d476060773398
Danilo Bargen <mail@dbrgn.ch>
2015-09-25 16:41:54 +0200

    Implemented single and double emphasis

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate ansi_term;
 use std::io::Write;
 use hoedown::{Markdown, Html, Buffer};
 use hoedown::renderer::{html, Render};
-use ansi_term::Colour::Yellow;
+use ansi_term::Colour::{White, Yellow};
 use ansi_term::Style;
 
 
@@ -16,15 +16,34 @@ impl Render for AnsiTerm {
 
     fn header(&mut self, output: &mut Buffer, content: &Buffer, level: i32) {
         let text = content.to_str().unwrap();
-        let header = format!("# {}", text);
+        let header = format!("# {}\n\n", text);
         let formatted = Yellow.bold().paint(&header);
         output.write(&formatted.to_string().into_bytes());
+    }
+
+    fn paragraph(&mut self, output: &mut Buffer, content: &Buffer) {
+        output.pipe(content);
+        output.write(&[0x0a]);
+    }
+
+    fn emphasis(&mut self, output: &mut Buffer, content: &Buffer) -> bool {
+        let text = content.to_str().unwrap();
+        let formatted = White.italic().paint(&text);
+        output.write(&formatted.to_string().into_bytes());
+        true // Why?
+    }
+
+    fn double_emphasis(&mut self, output: &mut Buffer, content: &Buffer) -> bool {
+        let text = content.to_str().unwrap();
+        let formatted = White.bold().paint(&text);
+        output.write(&formatted.to_string().into_bytes());
+        true // Why?
     }
 
 }
 
 fn main() {
-    let doc = Markdown::new("# hey there!\nsome _emphasis_ *is* required.");
+    let doc = Markdown::new("# hey there!\nSome _emphasis_ **is** required.");
     let mut terminal = AnsiTerm { name: "uxterm".to_string() };
     
     println!("{}", terminal.render(&doc).to_str().unwrap());
